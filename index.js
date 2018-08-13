8cd2438f73c7eeadcfb4ecdcebe6e4da8fa2d765
Unknown <kacper.potyrala@gmail.com>
2018-08-13 23:03:55 +0200

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+// const config = require('./config')
+const token = "NDc4NjY3NzkwOTQxODE0Nzg3.DlOCkA.CpqyVESgduKQV-Ow1fx9DA3y8wQ"
+// commands import
+
+// variables
+
+const client = new Discord.Client({ disableEveryone: true })
+
+client.on('ready', () => {
+  console.log(`\nInitialized on ${new Date().toUTCString()}.`)
+  client.user.setPresence({ game: 'DevTycoon bugs' , type: 3 })
+})
+
+// commands handler
+client.commands = new Discord.Collection()
+
+
+/* client.on('message', async (message) => {
+  const command = message.content.slice(config.prefix.length)
+  
+}) */
+
+client.login(token)
