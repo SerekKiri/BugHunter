b487471226f22d5f94002f73fea9b3f17e732e2f
Unknown <kacper.potyrala@gmail.com>
2018-08-13 23:05:32 +0200

    Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
+
+client.login(process.env.BOT_TOKEN);
 // const config = require('./config')
-const token = "NDc4NjY3NzkwOTQxODE0Nzg3.DlOCkA.CpqyVESgduKQV-Ow1fx9DA3y8wQ"
+//const token = "NDc4NjY3NzkwOTQxODE0Nzg3.DlOCkA.CpqyVESgduKQV-Ow1fx9DA3y8wQ"
 // commands import
 
 // variables
