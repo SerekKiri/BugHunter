2fb08bf4cee1293056410fe798755ed163b53d64
Unknown <kacper.potyrala@gmail.com>
2018-08-14 19:49:57 +0200

    Remove ussles options

--- javascript/src/commands/report.js
+++ javascript/src/commands/report.js
@@ -3,16 +3,11 @@ function bug(message) {
     // variables
     let mes = message.content.slice(11).split('|')
     let img = message.content.split('| ')
-    let video
     let text = ''
-    if (img[1].startsWith('https://www.youtube.com/watch?v=')) {
-      video = img[1].split('https://www.youtube.com/watch?v=')
-      text = '**Link:**' + '\n' + img[1]
-    } else if (img[1].startsWith('https://youtu.be/')) {
-      video = img[1].split('https://youtu.be/')
-      text = '**Link:**' + '\n' + img[1]
+    if (!(img[1] === undefined)) {
+      text = img[1]
     } else {
-      video = ''
+      text = 'There is no link'
     }
     const time = new Date(message.createdTimestamp)
     const aut = message.author.username + '#' +  message.author.discriminator
@@ -26,26 +21,23 @@ function bug(message) {
      }
 
     if (mes[0].length < 10) {
-        message.reply('You need to describe bug, min 10 letters')
+        message.reply('You need to describe your problem with minimum 10 letters')
     } else {
     try {
         const embed = {
             title: `Report from ${aut}`,
-            description: '**Report text:**\n' + mes[0] + '\n\n' + text,
+            description: '**Report text:**\n' + mes[0] + '\n\n' + '**Link:**' + '\n' + text,
             timestamp: time,
             color: 15859772,
             footer: {
               icon_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCaZDPW_-dzsNbodLC5KiAT3BlciGOybFjsUZPRSKI2u7tuQg2BQ',
               text: 'BugHunter by R1SK and Kiritito',
             },
-           "image": {
-              "url": `https://img.youtube.com/vi/${video[1]}/0.jpg`
-            },
             thumbnail: {
               url: avatar,
             }
           }
-          message.guild.channels.find("name", chan).send({ embed })
+          message.guild.channels .find(v => v.name === chan).send({ embed })
           // message.channel.send({ embed })
       } catch (err) {
         console.log(err)
