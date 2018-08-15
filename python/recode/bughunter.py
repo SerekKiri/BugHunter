ab9406b44abe7fe731994d8ebbe723d7b42fe197
Wandal <easy7331@wp.pl>
2018-08-15 17:33:31 +0200

    So close to finish... but still far for me.

--- python/recode/bughunter.py
+++ python/recode/bughunter.py
@@ -31,7 +31,7 @@ class JConfig:
 	def __init__(self, cfgName):
 		self.fileName = cfgName
 	def read(self):
-		self.file = open(self.fileName, 'r+')
+		self.file = open(self.fileName, 'r')
 		self.jsonFile = json.load(self.file)
 		self.file.close()
 	def readValue(self, name):
@@ -44,14 +44,11 @@ class JConfig:
 	def readServers(self):
 		serverList = []
 		for server in self.jsonFile['servers']:
-			print(server)
 			channelList = []
 			for channel in server['channels']:
-				print(channel)
 				channelList.append(channel)
 			sServer = CfgServer()
 			sServer.initialise(server['name'], channelList)
-			print('dupa', sServer.name, sServer.channelList[0], sServer.channelList[1])
 			serverList.append(sServer)
 		return serverList
 
@@ -67,30 +64,64 @@ class BugHunter:
 	cmdPrefix = ''
 	argSeparator = ''
 	client = discord.Client()
-	#SubFunctions
-
 	#Misc functions
-	def findAllOccurrences(s, ch):
-		return [i for i, letter in enumerate(s) if letter == ch]
+	def isServerAtList(self, serverName):
+		for server in self.serverList:
+			if(server.name == serverName):
+				return True
+		return False
+	def getAllBotChannelsAtServer(self, server):
+		parsedServer = 0
+		for x in self.serverList:
+			if(x.name == server.name):
+				parsedServer = x
+		channels = []
+		for channel in server.channels:
+			for x in parsedServer.channelList:
+				if(channel.name == x):
+					channels.append(discord.utils.get(self.client.get_all_channels(), server__name=server.name, name=channel.name))
+		return channels
+	def findAllOccurrences(self, s, ch):
+		idxs = []
+		for i in range(len(s)):
+			if(s[i] == ch):
+				idxs.append(i)
+		return idxs
+	def findCharOffset(self, string, char, offset):
+		for x in range(offset, len(string)):
+			if(string[x] == char):
+				return x
+		return -1
 	def isCommand(self, message):
 		return message.content.startswith(self.cmdPrefix)
 	def commandAndArgsFromMessage(self, message):
 		hasParams = False
 		parameterIndexes = []
 		paramCount = 0
-		if(message.content.find(argSeparator)):
+		if(message.content.find(self.argSeparator) != -1):
+			hasParams = True
+		if(self.findCharOffset(message.content, ' ', len(self.cmdPrefix) + 2) != -1):
 			hasParams = True
 		if(hasParams == False):
-			command = message.content[len(self.cmdPrefix) : len(message.content)]
+			command = message.content[len(self.cmdPrefix) + 1 : len(message.content)]
 			return ParsedCommand(command)
 		else:
-			parameterIndexes = findAllOccurrences(message.content, argSeparator)
+			parameterIndexes = self.findAllOccurrences(message.content, self.argSeparator)
 			paramCount = len(parameterIndexes)
-			command = message.content[len(self.cmdPrefix) : parameterIndexes[0]]
+			command = message.content[len(self.cmdPrefix) + 1 : self.findCharOffset(message.content, ' ', len(self.cmdPrefix) + 2)]
 			args = []
-			for x in range(0, argCount):
-				args.append(message.content[parameterIndexes[x - 1] : parameterIndexes[x]])
-			return ParsedCommand(command, command, True, args)
+			if(paramCount == 0):
+				args.append(message.content[len(self.cmdPrefix) + len(command) + 2 : len(message.content)])
+				return ParsedCommand(command, True, args)
+			for x in range(0, paramCount + 1):
+				if(x == 0):
+					args.append(message.content[len(self.cmdPrefix) + len(command) + 2 : parameterIndexes[x]])
+					continue
+				if(x == paramCount):
+					args.append(message.content[parameterIndexes[x - 1] + 2 : len(message.content)])
+					continue
+				args.append(message.content[parameterIndexes[x - 1] + 2 : parameterIndexes[x] - 1])
+			return ParsedCommand(command, True, args)
 	def loadConfig(self):
 		print('Loading config...')
 		config = JConfig('cfg\\config.json')
@@ -103,22 +134,44 @@ class BugHunter:
 		self.argSeparator = config.readValue('argSeparator')
 		self.normalUser = config.readBoolean('normalUser')
 		self.serverList = data.readServers()
-		print(self.serverList)
 		print('Config loaded')
+	#SubFunctions
+	def sendEmbedAtMultipleChannels(self, channels, embed):
+		for channel in channels:
+			channel.send(embed=embed)
+	def createHelpEmbed(self):
+		embed=discord.Embed(title='Help', description='Usage -> bug report bug description (additional -> screenshot)')
+		embed.set_author(name='BugHunter', url='https://github.com/quritto/DenisAskedMeToDoThisBotForReportBugs', icon_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCaZDPW_-dzsNbodLC5KiAT3BlciGOybFjsUZPRSKI2u7tuQg2BQ')
+		embed.set_footer(text='BugHunter by Kiritito and R1SK')
+		return embed
+	def createBugReportEmbed(self, message, parsedMsg):
+		title = message.author.mention + ' ' + 'sent report'
+		embed=discord.Embed(title=title, description=parsedMsg.args[0], color=0xff0000)
+		embed.set_author(name='BugHunter', url='https://github.com/quritto/DenisAskedMeToDoThisBotForReportBugs',icon_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCaZDPW_-dzsNbodLC5KiAT3BlciGOybFjsUZPRSKI2u7tuQg2BQ')
+		embed.set_footer(text='BugHunter by Kiritito and R1SK')
+		return embed
 	#Main functions
 	def initialise(self):
+		print('BugHunter by R1SK & Kiritito')
 		self.loadConfig()
+	def handleCommands(self, message, parsedMessage):
+		if(parsedMessage.command == 'report'):
+			self.sendEmbedAtMultipleChannels(self.getAllBotChannelsAtServer(message.guild), self.createBugReportEmbed(message, parsedMessage))
+		else:
+			self.sendEmbedAtMultipleChannels(self.getAllBotChannelsAtServer(message.guild), self.createHelpEmbed())
 	#Event handlers
 	async def onReady(self):
-		print('BugHunter by R1SK & Kiritito')
 		print('Logged in as', self.client.user.name, 'with ID', self.client.user.id)
-		isReady = True
+		self.isReady = True
 	async def onMessage(self, message):
 		if(self.isReady == False):
 			return
-		if(self.isCommand(message)):
-			msg = commandAndArgsFromMessage(message)
-			print(msg.command, msg.args, msg.hasArgs, msg.argCount)
+		if(self.isCommand(message) and self.isServerAtList(message.guild.name)):
+			msg = self.commandAndArgsFromMessage(message)
+			self.handleCommands(message, msg)
+			print('parameter count ----->', msg.argCount)
+			print('command ----->', msg.command)
+			print('args ----->', msg.args)
 
 Main = BugHunter() 
 
